ccfbc88a2883505bc362465f4ab62010a9bfac31
Gyulo94 <qkrcjfrb112@gmail.com>
2023-12-18 21:24:48 +0900

    query string 처리하기

--- index.js
+++ index.js
@@ -8,14 +8,21 @@ app.use(express.json())
 // localhost:8000
 app.listen(8000, () => {
     console.log("서버 가동 포트번호 8000");
-})
+});
 
 // 기본 주소 요청
 // localhost:8000
-app.get("/", (req, res) => {
+app.get("/", () => {
     console.log("기본주소 요청");
 });
 
 app.get("/hello", () => {
     console.log("hello 주소 요청");
-})
+});
+
+// query string 받기
+app.get("/qs", (req) => {
+    console.log(req.query);
+    console.log(req.query.p1);
+    console.log(req.query.p2);
+});
