b8349d20c078a08ccb32271e01c229ef78e6fd0c
Gyulo94 <qkrcjfrb112@gmail.com>
2023-12-18 21:15:22 +0900

    get 요청 처리하기

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ app.use(express.json())
 app.listen(8000, () => {
     console.log("서버 가동 포트번호 8000");
 })
+
+// 기본 주소 요청
+// localhost:8000
+app.get("/", (req, res) => {
+    console.log("기본주소 요청");
+});
+
+app.get("/hello", () => {
+    console.log("hello 주소 요청");
+})
